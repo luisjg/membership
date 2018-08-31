3e4c60a4377c5f13e9a34add3104f3cbd4ee2c67
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2018-08-31 12:52:21 -0700

    fix return

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -23,36 +23,9 @@ class FacultyController extends Controller {
             ->get()
             ->toArray();
 
-<<<<<<< HEAD
         $size = count($results);
 
         return $this->jsonResponse($results, $size);
-=======
-            array_push($results, $temp);
-        }
-        $size = count($results);
-
-        return $this->jsonResponse($results, $size);
-    }
-
-    public function getClassesWithTerm($term, $email){
-      $user = User::email($email)->first();
-      $classes = ClassMemberships::membersId($user->user_id)
-          ->instructorRole()
-          ->pluck('classes_id');
-
-      $data = [];
-      foreach($classes as $class){
-        $temp = Classes::classesId($class)
-        ->termId($term)
-        ->first();
-        if($temp != null){
-          array_push($data, $temp);
-        }
-      }
-      $size = count($data);
-      return $this->jsonResponse($data, $size);
->>>>>>> MEM-7
     }
 
     public function getClassesWithTerm($term, $email){
@@ -65,10 +38,7 @@ class FacultyController extends Controller {
 
       $data = Classes::classesId($classes)->get()->toArray();
 
-<<<<<<< HEAD
       $size = count($data);
       return $this->jsonResponse($data, $size);
     }
-=======
->>>>>>> MEM-7
 }
