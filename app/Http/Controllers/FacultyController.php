22234063347ab39f67471403379c79d46014c9d5
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2018-09-04 17:58:48 -0700

    add documentation to code i.e. comments

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -18,14 +18,19 @@ class FacultyController extends Controller {
             ->instructorRole()
             ->pluck('classes_id')
             ->toArray();
+        //nemo.classMemberships, query by user_id, filter by role_position
+        //and pluck classes_id
 
         $results = Classes::classesId($classes)
             ->get()
             ->toArray();
+        //bedrock.events, query by classes, store every row in array
 
         $size = count($results);
+        //total number of classes
 
         return $this->jsonResponse($results, $size);
+        // return response with JSON header
     }
 
     public function getClassesWithTerm($term, $email){
@@ -35,10 +40,16 @@ class FacultyController extends Controller {
           ->where('term_id', $term)
           ->pluck('classes_id')
           ->toArray();
+          //nemo.classMemberships, query by user_id, filter by role_position,
+          //filter by term_id, and pluck classes_id
 
       $data = Classes::classesId($classes)->get()->toArray();
+      //bedrock.events, query by classes, store every row in array
 
       $size = count($data);
+      //total number of classes
+
       return $this->jsonResponse($data, $size);
+      //return response with JSON header
     }
 }
