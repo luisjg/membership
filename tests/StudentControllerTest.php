5e85707b99c6db41c5d596c29167fd10e8bfba2c
luisjg <luis.guzman@csun.edu>
2020-11-18 15:25:05 -0800

    Fix the unit tests.

--- tests/StudentControllerTest.php
+++ tests/StudentControllerTest.php
@@ -1,27 +1,23 @@
 <?php
 
-
-use App\Http\Controllers\StudentController;
-use App\routes\web;
-
 class StudentControllerTest extends TestCase {
-    
+
     /**
      * @test
      */
-    public function testGetStudentClasses(){
+    public function testGetStudentClasses() :void{
 
         $response = $this->get('/student/nr_seth.gray.74/classes?api_key=SANDBOX');
 
-        $this->assertEquals(200, $response->response->getData()->status);
+        $this->assertEquals(200, $response->getStatus());
 
     }
 
-    public function testGetStudentClasseswithTerm(){
+    public function testGetStudentClasseswithTerm() :void{
 
         $response = $this->get('/student/nr_seth.gray.74/classes/2157?api_key=SANDBOX');
 
-        $this->assertEquals(200, $response->response->getData()->status);
+        $this->assertEquals(200, $response->getStatus());
 
     }
 }
